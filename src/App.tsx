6f2c3b654f86cceb75a752f793191d6ee41740bd
chanhihi <chanhihi55@gmail.com>
2024-06-18 17:36:04 +0900

    feat: app, diary, mypage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Diary from './diary/DiaryPage';
+import MyPage from './mypage/MyPage';
 
 const App = () => {
+  const Tab = createBottomTabNavigator();
+
   return (
     <RecoilRoot>
-      <View>
-        <Text>Hello World!</Text>
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Diary"
+            component={Diary}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MyPage"
+            component={MyPage}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="account" color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </RecoilRoot>
   );
 };
