b9d10a44b1041bb63c51431438839a5ced932190
chanhihi <chanhihi55@gmail.com>
2024-06-19 17:56:19 +0900

    feat: ai letter navigator

---
+++ src/navigators/AiLetterStackNavigator.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import AiLetter from '@screens/ai/AiLetter';
+
+const Stack = createStackNavigator();
+
+const AiLetterStackNavigator = () => (
+  <Stack.Navigator
+    initialRouteName="Details"
+    screenOptions={{
+      ...TransitionPresets.SlideFromRightIOS,
+      headerShown: false,
+    }}
+  >
+    <Stack.Screen
+      options={{
+        transitionSpec: {
+          open: {
+            animation: 'spring',
+            config: {
+              stiffness: 2000,
+              damping: 1000,
+            },
+          },
+          close: {
+            animation: 'spring',
+            config: {
+              stiffness: 1000,
+              damping: 500,
+            },
+          },
+        },
+      }}
+      name="Details"
+      component={AiLetter}
+    />
+  </Stack.Navigator>
+);
+
+export default AiLetterStackNavigator;
