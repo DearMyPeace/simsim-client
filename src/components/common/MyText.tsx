4ff59bff6a1da19176c7420a69c08eed2025917b
hyobb109 <hyobb109@gmail.com>
2024-06-20 06:23:57 +0900

    fix: MyText 수정

--- src/components/common/MyText.tsx
+++ src/components/common/MyText.tsx
@@ -8,11 +8,7 @@ interface MyTextProps extends TextProps {
 
 // android 호환성 문제로 fontweight은 사용 불가
 const MyText = ({ font = 'Inder-Regular', size = 14, style, ...rest }: MyTextProps) => {
-  return (
-    <Text style={[{ fontFamily: font, fontSize: size }, style]} {...rest}>
-      My Text
-    </Text>
-  );
+  return <Text style={[{ fontFamily: font, fontSize: size }, style]} {...rest} />;
 };
 
 export default MyText;
