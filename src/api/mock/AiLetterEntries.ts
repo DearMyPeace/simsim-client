4df89f1603fbbcbc2617a96170c74c74c1ce0176
chanhihi <chanhihi55@gmail.com>
2024-06-19 17:56:08 +0900

    test: mock ai letter

---
+++ src/api/mock/AiLetterEntries.ts
@@ -0,0 +1,101 @@
+// AiLetterEntries.js
+const AiLetterEntries = [
+  {
+    id: '1',
+    date: '2024-04-25',
+    summary: 'Today was great!',
+    content: 'Today was great because I went hiking and enjoyed the outdoors.',
+  },
+  {
+    id: '2',
+    date: '2024-05-21',
+    summary: 'Challenging day at work',
+    content: 'Work was tough today, but I managed to get through it.',
+  },
+  {
+    id: '3',
+    date: '2024-06-1',
+    summary: 'Today was great!',
+    content: 'Today was great because I went hiking and enjoyed the outdoors.',
+  },
+  {
+    id: '4',
+    date: '2024-06-2',
+    summary: 'Challenging day at work',
+    content: 'Work was tough today, but I managed to get through it.',
+  },
+  {
+    id: '5',
+    date: '2024-06-3',
+    summary: 'Today was great!',
+    content: 'Today was great because I went hiking and enjoyed the outdoors.',
+  },
+  {
+    id: '6',
+    date: '2024-06-4',
+    summary: 'Challenging day at work',
+    content: 'Work was tough today, but I managed to get through it.',
+  },
+  {
+    id: '7',
+    date: '2024-06-10',
+    summary: 'Today was great!',
+    content: 'Today was great because I went hiking and enjoyed the outdoors.',
+  },
+  {
+    id: '8',
+    date: '2024-06-11',
+    summary: 'Challenging day at work',
+    content: 'Work was tough today, but I managed to get through it.',
+  },
+  {
+    id: '9',
+    date: '2024-06-15',
+    summary: 'Today was great!',
+    content: 'Today was great because I went hiking and enjoyed the outdoors.',
+  },
+  {
+    id: '10',
+    date: '2024-06-16',
+    summary: 'Challenging day at work',
+    content: 'Work was tough today, but I managed to get through it.',
+  },
+  {
+    id: '11',
+    date: '2024-06-17',
+    summary: 'Today was great!',
+    content: 'Today was great because I went hiking and enjoyed the outdoors.',
+  },
+  {
+    id: '12',
+    date: '2024-06-18',
+    summary: 'Challenging day at work',
+    content: 'Work was tough today, but I managed to get through it.',
+  },
+  {
+    id: '13',
+    date: '2024-06-19',
+    summary: 'Today was great!',
+    content: 'Today was great because I went hiking and enjoyed the outdoors.',
+  },
+  {
+    id: '14',
+    date: '2024-06-20',
+    summary: 'Challenging day at work',
+    content: 'Work was tough today, but I managed to get through it.',
+  },
+  {
+    id: '15',
+    date: '2024-06-21',
+    summary: 'Today was great!',
+    content: 'Today was great because I went hiking and enjoyed the outdoors.',
+  },
+  {
+    id: '16',
+    date: '2024-06-22',
+    summary: 'Challenging day at work',
+    content: 'Work was tough today, but I managed to get through it.',
+  },
+];
+
+export default AiLetterEntries;
