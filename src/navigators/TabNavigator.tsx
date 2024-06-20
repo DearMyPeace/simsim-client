affff27e61bd0f08310f9660cbf912923dc973fe
hyobb109 <hyobb109@gmail.com>
2024-06-21 02:32:12 +0900

    feat: 하단 탭 바 아이콘 정의

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Diary from '@navigators/DiaryStackNavigator';
 import MyPage from '@screens/mypage/MyPage';
 import AiLetter from '@navigators/AiLetterStackNavigator';
-
-const AiLetterIcon = ({ color }) => (
-  <MaterialCommunityIcons name="email-outline" color={color} size={26} />
-);
 import PieceChip from '@components/diary/header/PieceChip';
 import SettingButton from '@components/diary/header/SettingButton';
+import { AiLetterIcon, CalendarIcon, PieceIcon, ShopIcon } from '@components/common/TabIcons';
 
 const TabNavigator = () => {
   const Tab = createBottomTabNavigator();
@@ -29,10 +25,6 @@ const TabNavigator = () => {
           backgroundColor: 'transparent',
         },
         headerLeftContainerStyle: { paddingLeft: 16 },
-        tabBarLabel: '달력',
-        tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="calendar-edit" color={color} size={26} />
-        ),
       }}
     >
       <Tab.Screen
@@ -40,9 +32,7 @@ const TabNavigator = () => {
         component={Diary}
         options={{
           tabBarLabel: '달력',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="calendar-edit" color={color} size={26} />
-          ),
+          tabBarIcon: CalendarIcon,
         }}
       />
       <Tab.Screen
@@ -50,7 +40,7 @@ const TabNavigator = () => {
         component={Diary}
         options={{
           tabBarLabel: '조각',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="home" color={color} size={26} />,
+          tabBarIcon: PieceIcon,
         }}
       />
       <Tab.Screen
@@ -58,7 +48,7 @@ const TabNavigator = () => {
         component={AiLetter}
         options={{
           headerShown: false,
-          tabBarLabel: '인공지능 편지',
+          tabBarLabel: '편지',
           tabBarIcon: AiLetterIcon,
         }}
       />
@@ -67,9 +57,7 @@ const TabNavigator = () => {
         component={MyPage}
         options={{
           tabBarLabel: '조각상점',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
-          ),
+          tabBarIcon: ShopIcon,
         }}
       />
     </Tab.Navigator>
