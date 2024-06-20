9c1283c306f5890ae623c57f021c3a3f64fe4954
chanhihi <chanhihi55@gmail.com>
2024-06-20 22:31:11 +0900

    fix: SafeAreaProvider

--- src/navigators/AiLetterStackNavigator.tsx
+++ src/navigators/AiLetterStackNavigator.tsx
@@ -1,40 +1,45 @@
 import React from 'react';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import AiLetter from '@screens/ai/AiLetter';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createStackNavigator();
 
 const AiLetterStackNavigator = () => (
-  <Stack.Navigator
-    initialRouteName="Details"
-    screenOptions={{
-      ...TransitionPresets.SlideFromRightIOS,
-      headerShown: false,
-    }}
-  >
-    <Stack.Screen
-      options={{
-        transitionSpec: {
-          open: {
-            animation: 'spring',
-            config: {
-              stiffness: 2000,
-              damping: 1000,
+  <SafeAreaProvider>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack.Navigator
+        initialRouteName="Details"
+        screenOptions={{
+          ...TransitionPresets.SlideFromRightIOS,
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          options={{
+            transitionSpec: {
+              open: {
+                animation: 'spring',
+                config: {
+                  stiffness: 2000,
+                  damping: 1000,
+                },
+              },
+              close: {
+                animation: 'spring',
+                config: {
+                  stiffness: 1000,
+                  damping: 500,
+                },
+              },
             },
-          },
-          close: {
-            animation: 'spring',
-            config: {
-              stiffness: 1000,
-              damping: 500,
-            },
-          },
-        },
-      }}
-      name="Details"
-      component={AiLetter}
-    />
-  </Stack.Navigator>
+          }}
+          name="Details"
+          component={AiLetter}
+        />
+      </Stack.Navigator>
+    </SafeAreaView>
+  </SafeAreaProvider>
 );
 
 export default AiLetterStackNavigator;
