a8c7a40f2a477a556b9ee5f9bc44b32f6dde6c91
chanhihi <chanhihi55@gmail.com>
2024-06-21 19:23:44 +0900

    fix: iOS Assets setting

--- react-native.config.js
+++ react-native.config.js
@@ -3,5 +3,12 @@ module.exports = {
     ios: {},
     android: {},
   },
-  assets: ['./src/assets/fonts'],
+  assets: ['./src/assets/fonts'], // 폰트 파일 경로
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null, // iOS에서 벡터 아이콘 복사를 비활성화합니다.
+      },
+    },
+  },
 };
