5dc810cee3dea3f52a113b29ae5ef0815d23a38a
chanhihi <chanhihi55@gmail.com>
2024-06-21 21:59:14 +0900

    refactor: ai letter 코드분리

---
+++ src/components/ai/AiLetterEntryContent.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { IAiLetterEntry } from '@type/IAiLetterEntry';
+import MyText from '@components/common/MyText';
+
+interface AiLetterEntryContentProps {
+  section: IAiLetterEntry;
+}
+
+const AiLetterEntryContent: React.FC<AiLetterEntryContentProps> = ({ section }) => {
+  return (
+    <View style={styles.content}>
+      <MyText style={styles.contentText}>{section.content}</MyText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    padding: 10,
+    borderRadius: 12,
+    backgroundColor: '#F1E2CC',
+  },
+  contentText: {
+    color: 'black',
+  },
+});
+
+export default AiLetterEntryContent;
