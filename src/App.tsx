ff37c8c665d231e754fe4fcc0b2ceae2a70e95cc
hyobb109 <hyobb109@gmail.com>
2024-06-23 04:41:48 +0900

    feat: queryClient default option 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,20 @@ import { createStackNavigator, TransitionPresets } from '@react-navigation/stack
 import TabNavigator from '@navigators/TabNavigator';
 import SettingPage from '@screens/setting/SettingPage';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { keepPreviousData, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const App = () => {
   const Tab = createBottomTabNavigator();
   const Stack = createStackNavigator();
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+        staleTime: 1000 * 60 * 5,
+        placeholderData: keepPreviousData,
+      },
+    },
+  });
 
   return (
     <RecoilRoot>
