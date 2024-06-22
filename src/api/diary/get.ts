eea31de3aead6353578f26386e6aab6fd49904f3
hyobb109 <hyobb109@gmail.com>
2024-06-22 23:00:41 +0900

    chore: 다이어리 비었을 때 문구 수정

--- src/api/diary/get.ts
+++ src/api/diary/get.ts
@@ -2,9 +2,10 @@ import React from 'react';
 import instance from '@api/axios';
 import { IDiaryListResponse } from '@type/Diary';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
-import { format, isToday } from 'date-fns';
+import { format, isFuture, isToday } from 'date-fns';
 
 export const fetchDiaryList = async (targetDate: string): Promise<IDiaryListResponse[]> => {
+  console.log('fetchDiaryList', targetDate);
   const response = await instance.get(`/diary/${targetDate}`);
   return response.data;
 };
@@ -13,16 +14,24 @@ export const useDiaryList = (targetDate: string) => {
   return useQuery({
     queryKey: ['diaryList', targetDate],
     queryFn: () => fetchDiaryList(targetDate),
+    enabled: !isFuture(new Date(targetDate)),
     select: (data) => {
       const diaryList = data.map((item) => ({
         id: item.diaryId.toString(),
         content: item.content,
         createdTime: format(new Date(item.createdDate), 'HH:mm a'),
       }));
-      const content = isToday(new Date(targetDate))
-        ? '오늘의 마음 조각을 남겨보세요'
-        : '작성된 마음 조각이 없습니다';
-      diaryList.length < 3 && diaryList.push({ id: 'newDiary', content, createdTime: '' });
+      const today = isToday(new Date(targetDate));
+      !today &&
+        diaryList.length === 0 &&
+        diaryList.push({ id: 'newDiary', content: '작성된 심심 기록이 없습니다', createdTime: '' });
+      today &&
+        diaryList.length < 3 &&
+        diaryList.push({
+          id: 'newDiary',
+          content: '오늘의 심심 기록을 남겨보세요',
+          createdTime: '',
+        });
       return diaryList;
     },
     placeholderData: keepPreviousData,
