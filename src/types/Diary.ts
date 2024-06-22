1f65fca34d048782ee42387cd3380d0875f6f5f8
hyobb109 <hyobb109@gmail.com>
2024-06-22 21:41:16 +0900

    chore: diary 관련 타입 추가

--- src/types/Diary.ts
+++ src/types/Diary.ts
@@ -1,14 +1,46 @@
 export interface IDiary {
   id: string;
-  createdTime: string;
   content: string;
+  createdTime: string;
 }
 
 export interface IDiaryList {
   diaryList: IDiary[];
 }
 
+export type DateStatus = 'FUTURE' | 'TODAY' | 'PAST';
+
+export interface IDiaryCarouselProps {
+  selectedDate: string;
+  dateStatus: DateStatus;
+}
+
 export interface IDiaryCardProps {
   createdTime: string;
   content: string;
+  dateStatus: DateStatus;
+}
+
+// api 요청 관련 타입
+export interface IDiaryListResponse {
+  diaryId: number;
+  userId: number;
+  content: string;
+  createdDate: Date;
+  modifiedDate: Date;
+}
+
+export interface IDiaryPostRequest {
+  userEmail: string;
+  createdTime: string; // yyyy-MM-dd HH:mm:ss
+  content: string;
+}
+
+export interface IDiaryEditRequest extends IDiaryPostRequest {
+  id: string;
+}
+
+export interface IDiaryDeleteRequest {
+  userEmail: string;
+  id: string;
 }
