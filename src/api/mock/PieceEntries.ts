7479386cd351cc54ac6e70b435c90943892e6931
chanhihi <chanhihi55@gmail.com>
2024-06-23 22:18:32 +0900

    test: mock piece entries

---
+++ src/api/mock/PieceEntries.ts
@@ -0,0 +1,38 @@
+const PieceEntries = [
+  {
+    name: '행복',
+    population: 30,
+    color: '#FECB52',
+    legendFontColor: '#333333',
+    legendFontSize: 15,
+  },
+  {
+    name: '슬픔',
+    population: 20,
+    color: '#54A3FF',
+    legendFontColor: '#333333',
+    legendFontSize: 15,
+  },
+  {
+    name: '화',
+    population: 7,
+    color: '#FF6B6B',
+    legendFontColor: '#333333',
+    legendFontSize: 15,
+  },
+  {
+    name: '힘듦',
+    population: 3,
+    color: '#A780F9',
+    legendFontColor: '#333333',
+    legendFontSize: 15,
+  },
+  {
+    name: '무난함',
+    population: 150,
+    color: '#4CAF50',
+    legendFontColor: '#333333',
+    legendFontSize: 15,
+  },
+];
+export default PieceEntries;
