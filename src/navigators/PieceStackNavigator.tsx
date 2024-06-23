9cfdb5fb34ff6884a84edef44351bed6953e1a28
chanhihi <chanhihi55@gmail.com>
2024-06-23 22:18:22 +0900

    feat: piece init

---
+++ src/navigators/PieceStackNavigator.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import Piece from '@screens/piece/Piece';
+
+const Stack = createStackNavigator();
+
+const PieceStackNavigator = () => (
+  <Stack.Navigator
+    initialRouteName="Details"
+    screenOptions={{
+      ...TransitionPresets.SlideFromRightIOS,
+      headerShown: false,
+    }}
+  >
+    <Stack.Screen
+      options={{
+        transitionSpec: {
+          open: {
+            animation: 'spring',
+            config: {
+              stiffness: 2000,
+              damping: 1000,
+            },
+          },
+          close: {
+            animation: 'spring',
+            config: {
+              stiffness: 1000,
+              damping: 500,
+            },
+          },
+        },
+      }}
+      name="Details"
+      component={Piece}
+    />
+  </Stack.Navigator>
+);
+
+export default PieceStackNavigator;
