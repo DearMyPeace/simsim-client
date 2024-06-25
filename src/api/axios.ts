247ec5be98472cf3dded831fd79d0bb2999e86d2
chanhihi <chanhihi55@gmail.com>
2024-06-25 12:32:36 +0900

    refactor: baseURL에 env

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://10.19.201.166:8080/api/v1',
+  baseURL: process.env.BASE_URL,
   headers: { 'X-Custom-Header': 'foobar', 'Content-Type': 'application/json' },
   timeout: 1000,
 });
