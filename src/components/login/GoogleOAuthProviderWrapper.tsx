a3b272fbe6d38de4ab95cd773b437c0a7825a003
hyobb109 <hyobb109@gmail.com>
2024-06-26 16:18:11 +0900

    fix: platform check 추가

--- src/components/login/GoogleOAuthProviderWrapper.tsx
+++ src/components/login/GoogleOAuthProviderWrapper.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Platform } from 'react-native';
 
 const clientId = process.env.GOOGLE_CLIENT_ID || '';
 
 const GoogleOAuthProviderWrapper = ({ children }) => {
+  if (Platform.OS !== 'web') {
+    return <>{children}</>;
+  }
   return <GoogleOAuthProvider clientId={clientId}>{children}</GoogleOAuthProvider>;
 };
 
