3c1c66e9a3d8ef8115ae2fccf1b16e7d175020b3
hyobicho <105159293+hyobb109@users.noreply.github.com>
2024-06-28 03:17:41 +0900

    fix: theme background 값 수정
    Co-authored-by: KIM CHANHEE <85754295+chanhihi@users.noreply.github.com>

--- src/utils/lightTheme.ts
+++ src/utils/lightTheme.ts
@@ -16,7 +16,7 @@ export const lightTheme = {
     onError: 'rgb(255, 255, 255)',
     errorContainer: 'rgb(255, 218, 214)',
     onErrorContainer: 'rgb(65, 0, 2)',
-    background: 'rgb(255, 251, 255)',
+    background: 'transparent',
     onBackground: 'rgb(31, 27, 22)',
     surface: 'rgb(255, 251, 255)',
     onSurface: 'rgb(31, 27, 22)',
