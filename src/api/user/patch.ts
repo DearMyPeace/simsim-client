cceef1bd9608af25f422a1d85ec5df6972d29d99
hyobb109 <hyobb109@gmail.com>
2024-06-29 15:49:29 +0900

    feat: ai persona 관련 api 연결

--- src/api/user/patch.ts
+++ src/api/user/patch.ts
@@ -1,7 +1,11 @@
 import instance from '@api/axios';
-import { IAiPersonaPatchRequest } from '@type/AiPersona';
+import { IAiPersonaData, IAiPersonaPatchRequest } from '@type/AiPersona';
 
-export const patchUserAiPersona = async ({ personaCode, userId }: IAiPersonaPatchRequest) => {
-  const response = await instance.patch(`/user/persona/${personaCode}/${userId}`);
+// todo: userId는 보내지 않는 것으로 수정
+export const patchUserAiPersona = async ({
+  personaCode,
+  userId,
+}: IAiPersonaPatchRequest): Promise<IAiPersonaData> => {
+  const response = await instance.patch(`/user/persona/${personaCode}`, userId);
   return response.data;
 };
--- src/hooks/setting/aiPatchHook.ts
+++ src/hooks/setting/aiPatchHook.ts
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { userAiPersonaStatus } from '@stores/userAiPersona';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { snackMessage } from '@stores/snackMessage';
 import { patchUserAiPersona } from '@api/user/patch';
+import { userStatus } from '@stores/user';
+import { IAiPersonaData } from '@type/AiPersona';
 
 const useAiPersonaChange = () => {
   const [userAiPersona, setUserAiPersona] = useRecoilState(userAiPersonaStatus);
   const [selectedPersonaCode, setSelectedPersonaCode] = useState<string>(userAiPersona.personaCode);
   const setSnackbar = useSetRecoilState(snackMessage);
+  const userId = useRecoilValue(userStatus);
+  const queryClient = useQueryClient();
+
   const changeAiPersona = useMutation({
-    mutationFn: (personaCode: string) =>
-      patchUserAiPersona({ personaCode: personaCode, userId: '1' }), // todo: userId 수정
-    onSuccess: (data) => {
-      console.log(data);
-      // todo: 응답 데이터 수정 후 수정
-      const name = data === 'T' ? '사고형' : '감정형';
-      setUserAiPersona({ personaCode: data, personaName: name });
-      setSnackbar(`${name} AI가 편지를 보냅니다`);
+    mutationFn: (personaCode: string) => patchUserAiPersona({ personaCode, userId }),
+    onSuccess: (data: IAiPersonaData) => {
+      queryClient.invalidateQueries({ queryKey: ['userInfo'] });
+      const { personaCode, personaName } = data;
+      setUserAiPersona({ personaCode, personaName });
+      setSnackbar(`${personaName} AI가 편지를 보냅니다`);
     },
     onError: (error) => {
       setSnackbar('AI 변경 중 오류가 발생했습니다');
