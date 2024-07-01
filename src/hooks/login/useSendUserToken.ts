0733da71029a646b5d06e8f782a3018189ff4e1b
chanhihi <chanhihi55@gmail.com>
2024-07-01 20:05:40 +0900

    refactor: login 앞 공통 비즈니스 로직 분리

---
+++ src/hooks/login/useSendUserToken.ts
@@ -0,0 +1,41 @@
+// useSendUserToken.ts
+import { useMutation } from '@tanstack/react-query';
+import { postUserGoogleToken, postUserAppleToken } from '@api/login/post';
+import { saveToken } from '@components/login/AuthService';
+import { useRecoilState } from 'recoil';
+import { authTokenState, userInfoState, isLoggedInState } from '@stores/login';
+
+interface AppleData {
+  authorization: string;
+  user: string | undefined;
+}
+
+interface TokenData {
+  token: string | AppleData;
+  type: 'google' | 'apple';
+}
+
+const useSendUserToken = () => {
+  const [, setAuthToken] = useRecoilState(authTokenState);
+  const [, setUserInfo] = useRecoilState(userInfoState);
+  const [, setIsLoggedIn] = useRecoilState(isLoggedInState);
+
+  return useMutation({
+    mutationFn: (data: TokenData) => {
+      if (data.type === 'google') {
+        return postUserGoogleToken(data);
+      } else {
+        return postUserAppleToken(data);
+      }
+    },
+    onSuccess: async (data: { accessToken: string }) => {
+      await saveToken(data.accessToken);
+      setIsLoggedIn(true);
+    },
+    onError: (error) => {
+      console.error(error.response.data.message);
+    },
+  });
+};
+
+export default useSendUserToken;
