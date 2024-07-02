34e18f41a6ef5245a7fe11db4dbd274646751b56
chanhihi <chanhihi55@gmail.com>
2024-07-02 21:57:37 +0900

    fix: platform 설정

--- src/components/common/TabIcons.tsx
+++ src/components/common/TabIcons.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Entypo from 'react-native-vector-icons/Entypo';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Platform, Image } from 'react-native';
+import { ITabBarIconProps } from '@type/ITabBarIconProps';
+import CalendarIconSVG from '@assets/images/dialy_1.svg';
+import PieceIconSVG from '@assets/images/tab_piece_2.svg';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
-import Feather from 'react-native-vector-icons/Feather';
-import MyIconButtons from '@components/common/MyIconButtons.tsx';
-import PieceIconPNG from '@assets/images/tab_piece_2.svg';
-import CalendarIconSVG from '@assets/images/dialy_1.svg';
-import { View, Text, Image } from 'react-native';
-import { red } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+import MyIconButtons from '@components/common/MyIconButtons';
+import { SvgUri } from 'react-native-svg';
 
-export const CalendarIcon = ({ color }: ITabBarIconProps) => (
-  <Image source={CalendarIconSVG} tintColor={color} style={{ width: 24, height: 24 }} />
-  // <MaterialCommunityIcons name="calendar-edit" color={color} size={26} />
-  // <Ionicons name="calendar-outline" color={color} size={24.5} />
-  // <FontAwesome name="sticky-note-o" color={color} size={24.5} />
-  // <FontAwesome name="calendar-o" color={color} size={24.5} />
-  // <Entypo name="pencil" color={color} size={24.5} />
-  // <FontAwesome name="spinner" color={color} size={26} />
-);
+export const CalendarIcon = ({ color }: ITabBarIconProps) => {
+  if (Platform.OS === 'web') {
+    return <Image source={CalendarIconSVG} tintColor={color} style={{ width: 24, height: 24 }} />;
+  }
+  return <CalendarIconSVG stroke={color} width={24} height={24} />;
+};
 
 export const AiLetterIcon = ({ color }: ITabBarIconProps) => {
   const currentHour = new Date().getHours();
@@ -31,10 +25,12 @@ export const ShopIcon = ({ color }: ITabBarIconProps) => (
   <AntDesign name="isv" color={color} size={24.5} />
 );
 
-export const PieceIcon = ({ color }: ITabBarIconProps) => (
-  // <FontAwesome6 name="puzzle-piece" color={color} size={26} />
-  <Image source={PieceIconPNG} tintColor={color} style={{ width: 34, height: 34 }} />
-);
+export const PieceIcon = ({ color }: ITabBarIconProps) => {
+  if (Platform.OS === 'web') {
+    return <Image source={PieceIconSVG} tintColor={color} style={{ width: 34, height: 34 }} />;
+  }
+  <PieceIconSVG stroke={color} width={34} height={34} />;
+};
 
 export const CloseIcon = ({ onPress }: { onPress: () => {} }) => (
   <MyIconButtons
