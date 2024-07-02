5caae6dac5677a7e1789e75de93fd2fb2c298a60
chanhihi <chanhihi55@gmail.com>
2024-07-02 18:29:26 +0900

    hotfix: axios header

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,18 +4,21 @@ import { getToken } from '@components/login/AuthService';
 const instance = axios.create({
   baseURL: process.env.BASE_URL,
   headers: { 'X-Custom-Header': 'foobar', 'Content-Type': 'application/json' },
-  timeout: 10000,
+  timeout: 1000,
 });
 
 instance.interceptors.request.use(
   async (config) => {
     console.log('Starting Request', JSON.stringify(config, null, 2));
-    if (config.url !== '/auth/google') {
-      const accessToken = await getToken();
-      if (accessToken) {
-        config.headers.Authorization = accessToken;
-        config.withCredentials = true;
-      }
+
+    if (config.url === '/auth/google') {
+      return config;
+    }
+
+    // 로그인 후 토큰 헤더에 추가
+    const accessToken = await getToken();
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   },
@@ -29,11 +32,17 @@ instance.interceptors.response.use(
     console.log('Response:', JSON.stringify(response, null, 2));
     return response;
   },
-  (error) => {
+  async (error) => {
     console.log('Error Response:', JSON.stringify(error, null, 2));
+
     if (error.message === 'Network Error' && error.config) {
       error.config.__isRetryRequest = true;
       return axios(error.config);
+    }
+
+    if (error.response && error.response.status === 401 && error.config.url !== '/auth/google') {
+      console.log('토큰 만료');
+      // TODO: 토큰 재발급 로직 추가
     }
     return Promise.reject(error);
   },
