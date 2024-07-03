48da6b301be28d045ea8b7773e73d2c37a0ab789
chanhihi <chanhihi55@gmail.com>
2024-07-03 15:42:37 +0900

    fix: duplicate

--- src/api/ai/get.ts
+++ src/api/ai/get.ts
@@ -2,28 +2,13 @@
 import instance from '@api/axios';
 import { IAiPersonaData } from '@type/AiPersona';
 
-export const fetchAiLetters = async (total) => {
-  const response = await instance.get(`/aiLetters`, {
-    params: {
-      total,
-    },
-  });
+export const fetchTodayAiLetters = async (total) => {
+  const response = await instance.get(`/aiLetters?total=${total}`);
   return response.data;
 };
 
-export const fetchTodayAiLetters = async (count) => {
-  const response = await instance.get(`/aiLetters?total=${count}`, {
-    params: {
-      count,
-    },
-  });
-  return response.data;
-};
-
-export const fetchNextAiLetter = async (offset, count) => {
-  const response = await instance.get(`/aiLetters?offset=${offset}&total=${count}`, {
-    params: { offset, count },
-  });
+export const fetchNextAiLetter = async (offset, total) => {
+  const response = await instance.get(`/aiLetters?offset=${offset}&total=${total}`);
   return response.data;
 };
 
