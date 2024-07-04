31fcaa7c9a1d92a2759316c487b2a743b838a142
hyobb109 <hyobb109@gmail.com>
2024-07-04 14:41:35 +0900

    fix: reissue 요청 시 config 수정

--- src/hooks/useAxiosInterceptors.ts
+++ src/hooks/useAxiosInterceptors.ts
@@ -9,7 +9,7 @@ const useAxiosInterceptors = () => {
   const { handleLogout } = useLogout();
 
   const reqConfig = async (config: InternalAxiosRequestConfig) => {
-    console.log('Starting Request', JSON.stringify(config, null, 2));
+    // console.log('Starting Request', JSON.stringify(config, null, 2));
 
     if (config.url === '/auth/google') {
       return config;
@@ -24,25 +24,33 @@ const useAxiosInterceptors = () => {
   };
 
   const reqErrorConfig = (error: any) => {
-    console.log('Error Request:', JSON.stringify(error, null, 2));
+    // console.log('Error Request:', JSON.stringify(error, null, 2));
     return Promise.reject(error);
   };
 
   const resConfig = (response: AxiosResponse) => {
-    console.log('Starting Response:', JSON.stringify(response, null, 2));
+    // console.log('Starting Response:', JSON.stringify(response, null, 2));
     return response;
   };
 
   const resErrorConfig = async (error: any) => {
-    console.log('Error Response:', JSON.stringify(error, null, 2));
-    if (error.config.url === '/auth/google') return Promise.reject(error);
+    // console.log('Error Response:', JSON.stringify(error, null, 2));
+    if (
+      error.config.url === '/auth/google' ||
+      error.config.url === '/auth/apple' ||
+      error.config.url === '/auth/reissue'
+    )
+      return Promise.reject(error);
     const originalRequest = error.config;
     // auth/google 제외한 요청 401(토큰 만료) 에러 시 토큰 재발급, 403(권한 없음) 에러 시 로그아웃
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       console.log('토큰 만료');
       axios
-        .post(`${process.env.BASE_URL}/auth/reissue`, null, originalRequest.headers)
+        .post(`${process.env.BASE_URL}/auth/reissue`, null, {
+          headers: { 'X-Custom-Header': 'foobar', 'Content-Type': 'application/json' },
+          withCredentials: true,
+        })
         .then(async (res) => {
           await saveToken(res.data.accessToken);
           originalRequest.headers.Authorization = res.data.accessToken;
@@ -52,7 +60,8 @@ const useAxiosInterceptors = () => {
           console.log('토큰 재발급 실패', err);
           handleLogout();
         });
-    } else if (error.response.status === 403) {
+    } else if (error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
       console.log('권한 없음');
       handleLogout();
     }
