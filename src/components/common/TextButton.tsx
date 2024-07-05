042d9d75a137a6db85577f2aaad91e93ea707896
hyobb109 <hyobb109@gmail.com>
2024-07-06 04:08:04 +0900

    feat: text button compact 추가

--- src/components/common/TextButton.tsx
+++ src/components/common/TextButton.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { Button, ButtonProps } from 'react-native-paper';
 
-const TextButton = ({ children, textColor = 'black', labelStyle, ...rest }: ButtonProps) => {
+const TextButton = ({
+  children,
+  compact = true,
+  textColor = 'black',
+  labelStyle,
+  ...rest
+}: ButtonProps) => {
   return (
     <Button
       mode="text"
-      compact
+      compact={compact}
       textColor={textColor}
       labelStyle={[{ fontFamily: 'GowunBatang-Regular', fontWeight: 400 }, labelStyle]}
       style={{
