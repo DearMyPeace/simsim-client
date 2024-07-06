1e81c1cb3c1cca07c91ab123e1d5c19ac70fd8a8
hyobb109 <hyobb109@gmail.com>
2024-07-07 01:28:23 +0900

    fix: isMarked 상태 삭제

--- src/hooks/diary/diaryHook.ts
+++ src/hooks/diary/diaryHook.ts
@@ -1,17 +1,13 @@
 import { useDiaryList } from '@api/diary/get';
 import { markedDateStatus } from '@stores/tense';
-import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 const useDiaryHook = (selectedDate: string) => {
   const markedDateSet = useRecoilValue(markedDateStatus);
-  const [isMarked, setIsMarked] = useState(false);
-
-  useEffect(() => {
-    setIsMarked(markedDateSet.has(selectedDate));
-  }, [selectedDate, markedDateSet]);
-
-  const { data, isPending, isError, isSuccess } = useDiaryList(selectedDate, isMarked);
+  const { data, isPending, isError, isSuccess } = useDiaryList(
+    selectedDate,
+    markedDateSet.has(selectedDate),
+  );
 
   return {
     data: data?.diaryList || [],
