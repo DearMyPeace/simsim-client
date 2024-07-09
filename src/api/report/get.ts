ae2fe54107f3194b5ff93db39a5ec4fe5058a57d
chanhihi <chanhihi55@gmail.com>
2024-07-09 23:24:51 +0900

    fix: 잘못된 uri 해결

--- src/api/report/get.ts
+++ src/api/report/get.ts
@@ -1,11 +1,11 @@
 import instance from '@api/axios';
 
 export const fetchWeekReport = async (targetDate: string) => {
-  const response = await instance.get(`/api/v1/report/week?${targetDate}`);
+  const response = await instance.get(`/report/week?${targetDate}`);
   return response.data;
 };
 
 export const fetchReportPNN = async (targetDate: string) => {
-  const response = await instance.get(`/api/v1/report/week/${targetDate}`);
+  const response = await instance.get(`/report/week/${targetDate}`);
   return response.data;
 };
