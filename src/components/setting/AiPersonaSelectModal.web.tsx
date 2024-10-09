c60aa5acc610d0e8134d7c8a97bff8fba408ea4e
hyobb109 <hyobb109@gmail.com>
2024-10-10 00:50:59 +0900

    feat: 라디오 버튼 구현

--- src/components/setting/AiPersonaSelectModal.web.tsx
+++ src/components/setting/AiPersonaSelectModal.web.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import MyText from '@components/common/MyText';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import MyModal from '@components/common/MyModal';
 import { IAiPersonaData } from '@type/AiPersona';
-import { RadioButton } from 'react-native-paper';
-import { fontLarge } from '@utils/Sizing';
 import useAiPersonaChange from '@hooks/setting/aiPatchHook';
+import MyRadioButton from '@components/common/MyRadioButton';
 
 interface IAiPersonaSelectProps {
   visible: boolean;
@@ -30,18 +28,17 @@ const AiPersonaSelectModal = ({ visible, setIsVisible, aiPersonaList }: IAiPerso
       transparent={true}
       animationType="fade"
     >
-      <ScrollView>
+      <ScrollView style={{ flexGrow: 1 }}>
         <View style={styles.modalContent}>
-          <RadioButton.Group onValueChange={onSelectAi} value={userInfo.personaCode}>
-            {aiPersonaList.map((persona) => (
-              <View key={persona.personaId} style={styles.row}>
-                <RadioButton value={persona.personaCode} />
-                <MyText size={fontLarge} style={styles.padding}>
-                  {persona.personaName}
-                </MyText>
-              </View>
-            ))}
-          </RadioButton.Group>
+          {aiPersonaList.map((persona) => (
+            <MyRadioButton
+              key={persona.personaId}
+              label={persona.personaName}
+              value={persona.personaCode}
+              selected={userInfo.personaCode === persona.personaCode}
+              onPress={onSelectAi}
+            />
+          ))}
         </View>
       </ScrollView>
     </MyModal>
@@ -53,26 +50,10 @@ const styles = StyleSheet.create({
     width: 200,
   },
   modalContent: {
-    padding: 10,
+    flex: 1,
+    paddingVertical: 10,
     flexDirection: 'column',
     justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  modalButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingVertical: 6,
-    width: '75%',
-  },
-  row: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  padding: {
-    paddingHorizontal: 10,
   },
 });
 
