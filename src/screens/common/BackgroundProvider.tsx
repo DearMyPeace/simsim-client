46c9e05e5ba711c0155dfd6a97f319440efe43a5
hyobb109 <hyobb109@gmail.com>
2024-10-26 21:06:40 +0900

    refactor: background public에서 삭제

--- src/screens/common/BackgroundProvider.tsx
+++ src/screens/common/BackgroundProvider.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Platform, StyleSheet, ImageBackground } from 'react-native';
 
 const BackgroundProvider = ({ children }) => {
-  if (Platform.OS === 'web') {
-    return <>{children}</>;
-  }
   return (
     <ImageBackground
       source={require('@assets/images/background.jpg')}
