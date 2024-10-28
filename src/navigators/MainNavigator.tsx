31fd51b3723c9066cd1132c92358128a562dae96
hyobb109 <hyobb109@gmail.com>
2024-10-28 17:45:34 +0900

    refactor: setting navigator lazy loading

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { useRecoilValue } from 'recoil';
 import { isLoggedInState } from '@stores/login';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import TabNavigator from '@navigators/TabNavigator';
 import LoginScreen from '@screens/login/LoginScreen';
-import SettingScreen from '@screens/setting/SettingScreen';
-import { CloseIcon } from '@components/common/TabIcons';
 import useAxiosInterceptors from '@hooks/useAxiosInterceptors';
 import useUserSetup from '@hooks/user/useUserSetup';
-import { fontLarge } from '@utils/Sizing';
-import SettingUserInfoScreen from '@screens/setting/SettingUserInfoScreen';
-import SettingTermsScreen from '@screens/setting/SettingTermsScreen';
+
+const SettingNavigator = lazy(() => import('./SettingNavigator'));
 
 const Stack = createStackNavigator();
 
@@ -42,44 +39,11 @@ const MainNavigator = () => {
               headerShown: false,
             }}
           />
-          <Stack.Group
-            screenOptions={({ navigation }) => ({
-              headerStyle: { backgroundColor: 'white' },
-              headerTitleStyle: {
-                fontFamily: 'GowunBatang-Regular',
-                fontSize: fontLarge,
-                paddingHorizontal: 10,
-              },
-              headerShadowVisible: false,
-              headerBackTitleVisible: false,
-              headerTintColor: 'black',
-              headerTitleAlign: 'left',
-              headerLeft: () => null,
-              headerRight: () => <CloseIcon onPress={navigation.goBack} />,
-            })}
-          >
-            <Stack.Screen
-              name="Settings"
-              component={SettingScreen}
-              options={{
-                title: '설정',
-              }}
-            />
-            <Stack.Screen
-              name="SettingUserInfo"
-              component={SettingUserInfoScreen}
-              options={{
-                title: '정보',
-              }}
-            />
-            <Stack.Screen
-              name="SettingTerms"
-              component={SettingTermsScreen}
-              options={{
-                title: '방침',
-              }}
-            />
-          </Stack.Group>
+          <Stack.Screen
+            name="Settings"
+            component={SettingNavigator}
+            options={{ headerShown: false }}
+          />
         </>
       ) : (
         <Stack.Screen
---
+++ src/navigators/SettingNavigator.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { CloseIcon } from '@components/common/TabIcons';
+import { fontLarge } from '@utils/Sizing';
+import SettingScreen from '@screens/setting/SettingScreen';
+import SettingUserInfoScreen from '@screens/setting/SettingUserInfoScreen';
+import SettingTermsScreen from '@screens/setting/SettingTermsScreen';
+
+const Stack = createStackNavigator();
+
+const SettingNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: 'white' },
+        headerTitleStyle: {
+          fontFamily: 'GowunBatang-Regular',
+          fontSize: fontLarge,
+          paddingHorizontal: 10,
+        },
+        headerShadowVisible: false,
+        headerBackTitleVisible: false,
+        headerTintColor: 'black',
+        headerTitleAlign: 'left',
+        headerLeft: () => null,
+        headerRight: () => <CloseIcon onPress={navigation.goBack} />,
+      })}
+    >
+      <Stack.Screen name="Settings" component={SettingScreen} options={{ title: '설정' }} />
+      <Stack.Screen
+        name="SettingUserInfo"
+        component={SettingUserInfoScreen}
+        options={{ title: '정보' }}
+      />
+      <Stack.Screen
+        name="SettingTerms"
+        component={SettingTermsScreen}
+        options={{ title: '방침' }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default SettingNavigator;
