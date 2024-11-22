eb634678aaa365ee3c3492ae1da3f51a8a95d402
hyobb109 <hyobb109@gmail.com>
2024-11-22 19:45:30 +0900

    test: keyword 내용 fetch

--- src/hooks/report/useReportData.ts
+++ src/hooks/report/useReportData.ts
@@ -1,8 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchMockReportData } from '@api/report/get';
+import { fetchMockReportData, fetchMockReportKeyword } from '@api/report/get';
 import { ICalendarModalDate } from '@type/Diary';
 
-const useReportData = (selectedDate: ICalendarModalDate) => {
+export const useReportData = (selectedDate: ICalendarModalDate) => {
   return useQuery({
     queryKey: ['report', selectedDate],
     queryFn: () =>
@@ -10,4 +10,19 @@ const useReportData = (selectedDate: ICalendarModalDate) => {
   });
 };
 
-export default useReportData;
+export const useReportKeyword = ({
+  selectedDate,
+  rank,
+}: {
+  selectedDate: ICalendarModalDate;
+  rank: number;
+}) => {
+  return useQuery({
+    queryKey: ['report', selectedDate],
+    queryFn: () =>
+      fetchMockReportKeyword({
+        targetDate: `${selectedDate.year}${selectedDate.month.toString().padStart(2, '0')}`,
+        rank: rank,
+      }),
+  });
+};
