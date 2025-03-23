79b4a6aa44c176974063f11fde65bfa28aa0a704
hyobb109 <hyobb109@gmail.com>
2025-03-23 10:52:42 +0900

    fix: useReportKeyword 수정

--- src/hooks/report/useReportData.ts
+++ src/hooks/report/useReportData.ts
@@ -1,14 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchMonthlyReport } from '@api/report/get';
 import { ICalendarModalDate } from '@type/Diary';
-import { IReportData } from '@type/IReport';
 
 export const useReportData = (selectedDate: ICalendarModalDate) => {
   return useQuery({
     queryKey: ['reportList', selectedDate],
     queryFn: () =>
       fetchMonthlyReport(`${selectedDate.year}${selectedDate.month.toString().padStart(2, '0')}`),
-    placeholderData: [],
   });
 };
 
@@ -19,13 +17,10 @@ export const useReportKeyword = ({
   selectedDate: ICalendarModalDate;
   rank: number;
 }) => {
-  return useQuery({
-    queryKey: ['reportKeyword', selectedDate, rank],
-    queryFn: () =>
-      fetchMonthlyReport(`${selectedDate.year}${selectedDate.month.toString().padStart(2, '0')}`),
-    select: (reportData: IReportData[]) => ({
-      keyword: reportData[rank - 1].keyword,
-      comment: reportData[rank - 1].comment,
-    }),
-  });
+  const { data: reportData } = useReportData(selectedDate);
+
+  return {
+    keyword: reportData?.[rank - 1]?.keyword,
+    comment: reportData?.[rank - 1]?.comment,
+  };
 };
--- src/screens/report/NewReportContent.tsx
+++ src/screens/report/NewReportContent.tsx
@@ -1,12 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StyleSheet, ScrollView, Pressable } from 'react-native';
 import MyText from '@components/common/MyText';
 import { fontMedium } from '@utils/Sizing';
 import { appColor1 } from '@utils/colors';
 import { ICalendarModalDate } from '@type/Diary';
 import { useReportKeyword } from '@hooks/report/useReportData';
-import ReportLoadingView from './ReportLoadingView';
-import ReportErrorView from './ReportErrorView';
 
 interface INewReportContentProps {
   selectedDate: ICalendarModalDate;
@@ -15,27 +13,15 @@ interface INewReportContentProps {
 }
 
 const NewReportContent = ({ selectedDate, rank, onPress }: INewReportContentProps) => {
-  const { data, isPending, isError } = useReportKeyword({ selectedDate, rank });
-
-  useEffect(() => {
-    console.log(`rank: ${rank}`);
-    console.log(data);
-  }, [rank, data]);
-
-  if (isPending) {
-    return <ReportLoadingView />;
-  }
-  if (isError || !data) {
-    return <ReportErrorView />;
-  }
+  const { keyword, comment } = useReportKeyword({ selectedDate, rank });
 
   return (
     <Pressable onPress={onPress} style={styles.container}>
       <MyText size={fontMedium} bold>
-        {data.keyword}
+        {keyword}
       </MyText>
       <ScrollView contentContainerStyle={{ maxHeight: 300 }} style={{ marginTop: 5 }}>
-        <MyText>{data.comment}</MyText>
+        <MyText>{comment}</MyText>
       </ScrollView>
     </Pressable>
   );
